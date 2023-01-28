d2826f4b90f9f37fa85efd0647f5c6d86878a767
arnabroy24 <103420052+arnabroy24@users.noreply.github.com>
2023-01-27 19:29:41 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import wmi,psutil
+import wmi,psutil,socket,uuid,re
 
 c = wmi.WMI()
 my_system = c.Win32_ComputerSystem()[0]
@@ -12,6 +12,8 @@ print(f"SystemFamily: {my_system.SystemFamily}")
 print(f"DNS Hostname: {my_system.DNSHostName}")
 print(f"Domain: {my_system.Domain}")
 print(f"UserName: {my_system.UserName}")
-print(f"Total Physical Memory: {int(my_system.TotalPhysicalMemory)//(1024*1024*1024)} GB")
+print(f"Total RAM: {round(int(my_system.TotalPhysicalMemory)/(1024*1024*1024))} GB")
 print(f"Workgroup: {my_system.Workgroup}")
-print(f"RAM:{str(round(psutil.virtual_memory().total / (1024.0 **3)))} GB")
+print(f"Hostname :{socket.gethostname()}")
+print(f"IP Address :{socket.gethostbyname(socket.gethostname())}")
+print(f"MAC Address:{':'.join(re.findall('..', '%012x' % uuid.getnode()))}")
