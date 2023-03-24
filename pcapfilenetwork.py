8381755e686633215a471a9b3d59b423c89b75a5
koulaks <103448896+koulaks@users.noreply.github.com>
2023-03-24 17:21:40 -0400

    Add files via upload

--- pcapfilenetwork.py
+++ pcapfilenetwork.py
@@ -1,17 +1,19 @@
 import scapy 
 import sys
 from scapy.all import *
-packets = rdpcap(r'C:\Users\aksha\OneDrive\Desktop\Tools\testpcpap.pcap')
-#packets.summary()  
-#for packet in packets:
- #   if packet.haslayer(UDP):
-        #print(packet.summary())  
-  #  if packet.haslayer(TCP):
-       #print(packet.show())  
-capture= sniff(count=50)
-#capture.summary()
-for packet in capture:
-    if packet.haslayer(UDP):
-        print(packet.summary())  
-    if packet.haslayer(TCP):
-       print(packet.show())
+
+capture = sniff(count=20)
+with open("Networkanalysis.txt", "w") as f:
+    for packet in capture:
+        if packet.haslayer(UDP):
+            f.write("UDP Packets\n")
+            f.write(str(packet))
+            f.write("\n\n")
+        elif packet.haslayer(TCP):
+            f.write("TCP Packets\n")
+            f.write(str(packet))
+            f.write("\n\n")
+        else:
+            f.write("All other packets\n")
+            f.write(str(packet))
+            f.write("\n\n")
